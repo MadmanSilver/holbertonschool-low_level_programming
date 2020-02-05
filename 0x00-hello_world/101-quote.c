6c191864bfa686d7dbe28128e440cc31c622b434
Sean Taylor <silverwolf4350@gmail.com>
2020-02-05 19:12:05 +0000

    Created all files including advanced.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Prints the end of the quote to stderr
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	return (1);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+0x00 C - Hello, World is a Holberton project to teach the basics of the C programming language.
