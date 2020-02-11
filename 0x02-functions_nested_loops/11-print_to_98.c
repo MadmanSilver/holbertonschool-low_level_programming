91f67936426d38d083b5e1437bb31475563723b6
Sean Taylor <silverwolf4350@gmail.com>
2020-02-11 16:49:45 +0000

    Fixed handling of numbers above 98.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Prints all numbers from n to 98
@@ -8,38 +8,17 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	for (i = n; i > 98; i--)
 	{
-		_putchar('9');
-		_putchar('8');
-	}
+		if (i != n)
+			printf(", ");
+		printf("%d", i);
+	} 
 	for (i = n; i < 99; i++)
 	{
-		if (i / 10 == 0)
-		{
-			if (i < 0)
-				_putchar('0' + i * -1);
-			else
-				_putchar('0' + i);
-		}
-		else
-		{
-                        if (i < 0)
-			{
-				_putchar('0' + i * -1 / 10);
-				_putchar('0' + i * -1 % 10);
-			}
-			else
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-		}
-		if (i < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i != n)
+			printf(", ");
+		printf("%d", i);
 	}
-	_putchar('\n');
+	printf("\n");
 }
