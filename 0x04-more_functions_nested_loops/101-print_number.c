e08b737dd99c5adca21654644b70f78bf0727481
Sean Taylor <silverwolf4350@gmail.com>
2020-02-14 03:31:40 +0000

    Fixed data corruption edge case

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,42 +8,37 @@
 void print_number(int n)
 {
 	int digits = 0, i;
-	unsigned int pos = 0;
-	unsigned int tmp = 0;
-	unsigned int rev = 0;
+	unsigned int pos = n, tmp = 0, rev = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		pos = -n;
 	}
-	else
-		pos = n;
-
 	tmp = pos;
-
 	if (n == 0)
 		_putchar('0');
-
 	while (pos > 0)
 	{
 		pos = pos / 10;
 		digits++;
 	}
-
 	pos = tmp;
-
+	if (pos % 10 > 3)
+	{
+		tmp = pos % 10;
+		pos -= pos % 10;
+	}
 	for (i = 0; i < digits; i++)
 	{
 		rev *= 10;
 		rev += pos % 10;
 		pos /= 10;
 	}
-
-	/*printf("Digits: %d, Pos: %u, Rev: %u\n", digits, pos, rev);*/
-
 	for (i = 0; i < digits; i++)
 	{
+		if (i == digits - 1)
+			rev += tmp;
 		_putchar('0' + rev % 10);
 		rev /= 10;
 	}
