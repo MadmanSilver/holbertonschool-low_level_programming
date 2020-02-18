e9b8e6476c64cd37b00bece940c86c4d688166f8
Sean Taylor <silverwolf4350@gmail.com>
2020-02-18 16:42:05 +0000

    Fixed string length calculations.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,23 @@
  */
 void puts_half(char *str)
 {
-	int i, c = sizeof(str) / 2 + 1;
+	int i, c = 0;
 
-	if (sizeof(str) % 2 != 0)
-		c = (sizeof(str) - 1) / 2;
+	while (str[c] != '\0')
+		c++;
 
-	for (i = c; str[i] != '\0'; i++)
+	c--;
+
+	if (c % 2 != 0)
+		c = (c - 1) / 2;
+	else
+		c = c / 2;
+
+	c++;
+
+	for (i = 0; i < c; i++)
 	{
-		_putchar(str[i]);
+		_putchar(str[i + c]);
 	}
 
 	_putchar('\n');
