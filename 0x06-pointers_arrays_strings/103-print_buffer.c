a8a84629440481b76b9976dfb6dd93c9db561a59
Sean Taylor <silverwolf4350@gmail.com>
2020-02-21 06:32:53 +0000

    Finished file for final advanced task.

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -8,22 +8,39 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i, j, k;
 	char *start = b;
 
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%ld: ", start - b);
-		for (j = 0; j < 10; j++)
+		if (size - i < 10)
+			k = size - i;
+		else
+			k = 10;
+		printf("%08lx: ", &b[i] - start);
+		for (j = 0; j < k; j++)
 		{
-			printf("%d", b[i + j]);
+			printf("%02x", b[i + j]);
 			if (j % 2 == 1)
 				putchar(' ');
 		}
-		for (j = 0; j < 10; j++)
+
+		for (; j < 10; j++)
 		{
-			printf("%c", b[j]);
+			printf("  ");
+			if (j % 2 == 1)
+				putchar (' ');
+		}
+
+		for (j = 0; j < k; j++)
+		{
+			if (b[i + j] > 31 && b[i + j] < 127)
+				printf("%c", b[i + j]);
+			else
+				putchar('.');
 		}
 		putchar('\n');
 	}
+	if (size < 1)
+		putchar('\n');
 }
