2db7434e7d7345577b086b64d68f5e47e6f2fa84
Sean Taylor <silverwolf4350@gmail.com>
2020-02-21 05:59:08 +0000

    Updated progress.

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -8,5 +8,22 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i, j;
+	char *start = b;
 
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%x: ", start - b);
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d", b[i + j]);
+			if (j % 2 == 1)
+				putchar(' ');
+		}
+		for (j = 0; j < 10; j++)
+		{
+			printf("%c", b[j]);
+		}
+		putchar('\n');
+	}
 }
