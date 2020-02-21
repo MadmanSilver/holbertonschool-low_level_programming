ac79bc4fcad49c93040e45e415ab074929fd5731
Sean Taylor <silverwolf4350@gmail.com>
2020-02-21 05:38:55 +0000

    Created files for advanced tasks 9 to 11.

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -11,62 +11,106 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, add;
-	int max = size_r;
-	int c1 = 0, c2 = 0;
-	int dif1 = 0, dif2 = 0;
+	int i, add, dif;
+	int j = size_r;
+	int c1 = count(n1), c2 = count(n2);
 
-	while (n1[c1] != '\0')
-		c1++;
-
-	while (n2[c2] != '\0')
-		c2++;
-
-/*	if (c1 > size_r - 1 || c2 > size_r - 1)
+	if (c1 >= size_r || c2 >= size_r)
 		return (0);
-*/
-	if (c1 > c2)
+	for (i = 0; i < j; i++)
 	{
-		i = c1;
-		dif1 = c1 - c2;
-	}
-	else if (c2 > c1)
-	{
-		i = c2;
-		dif2 = c2 - c1;
-	}
-	else
-	{
-		i = c1;
-	}
-
-	for (i = i - 1; i >= 0; i--, max--)
-	{
-		if (i - dif1 < 0)
+		if (c1 > c2)
 		{
-			r[i] = n1[i];
+			j = c1;
+			dif = c1 - c2;
+			if (i < dif)
+				add = n1[i] - '0';
+			else
+				add = (n1[i] - '0') + (n2[i - dif] - '0');
 		}
-		else if (i - dif2 < 0)
+		else if (c2 > c1)
 		{
-			r[i] = n2[i];
+			j = c2;
+			dif = c2 - c1;
+			if (i < dif)
+				add = n2[i] - '0';
+			else
+				add = (n2[i] - '0') + (n1[i - dif] - '0');
 		}
 		else
 		{
-			add += (n1[i - dif2] - '0') + (n2[i - dif1] - '0');
-			r[i] = add % 10;
+			j = c1;
+			add = (n1[i] - '0') + (n2[i] - '0');
 		}
+		r[i] = add + '0';
+	}
+	return (carryTheOne(r, j, size_r));
+}
 
+/**
+ * shiftR - shifts all characters in a string to the right once
+ * @s: string to shift
+ * @c: length of s
+ */
+void shiftR(char *s, int c)
+{
+	int i;
+
+	for (i = c; i > 0; i--)
+	{
+		s[i] = s[i - 1];
+	}
+
+	s[0] = '0';
+}
+
+/**
+ * count - counts length of a string
+ * @s: string to count
+ *
+ * Return: length of the string
+ */
+int count(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+
+	return (c);
+}
+
+/**
+ * carryTheOne - carries the one
+ * @r: needs ones carried
+ * @j: current length
+ * @size_r: max size of r
+ *
+ * Return: pointer to r
+ */
+char *carryTheOne(char *r, int j, int size_r)
+{
+	int i, add;
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		add = r[i] - '0';
 		if (add > 9)
 		{
-			if (max == 0)
-				return (0);
-			add = 1;
-		}
-		else
-		{
-			add = 0;
+			if (i == 0)
+			{
+				shiftR(r, j);
+				if (size_r <= j + 1)
+					return (0);
+				i++;
+				j++;
+			}
+			r[i - 1] += 1;
+			r[i] = (add % 10) + '0';
 		}
 	}
+
+	r[j] = '\0';
 
 	return (r);
 }
