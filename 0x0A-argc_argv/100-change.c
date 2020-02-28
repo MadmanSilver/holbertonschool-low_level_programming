1b3ad92bc4c6c39fc2cacbb80a003fb2924cbbc3
Sean Taylor <silverwolf4350@gmail.com>
2020-02-28 22:51:33 +0000

    Created all files for advanced tasks.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints minimum number of coins to make change for an amount
+ * @argc: num of args
+ * @argv: array of args
+ *
+ * Return: 1 if error, 0 otherwise
+ */
+int main(int argc, char* argv[])
+{
+	int num;
+	int count = 0;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+
+	while (num > 0)
+	{
+		if (!(num - 25 < 0))
+			num -= 25;
+		else if (!(num - 10 < 0))
+			num -= 10;
+		else if (!(num - 5 < 0))
+			num -= 5;
+		else if (!(num - 2 < 0))
+			num -= 2;
+		else
+			num -= 1;
+		count++;
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
