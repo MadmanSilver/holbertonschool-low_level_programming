9488c819e0377e7f2c38e3cf08e0de1f1f6a795c
Sean Taylor <silverwolf4350@gmail.com>
2020-03-04 05:24:40 +0000

    Added check for malloc failing.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,6 +19,9 @@ int **alloc_grid(int width, int height)
 
 	p = (int **)malloc(sizeof(int *) * height);
 
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		p[i] = (int *)malloc(sizeof(int) * width);
