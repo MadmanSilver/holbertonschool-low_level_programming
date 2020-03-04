3729c4affc1bc62634649029b8d00e4c96a81332
Sean Taylor <silverwolf4350@gmail.com>
2020-03-04 23:54:27 +0000

    Created all files for advanced tasks.

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into an array of words
+ * @str: string to split
+ *
+ * Return: pointer to array
+ */
+char **strtow(char *str)
+{
+	int i, j, k = 0;
+	int nw = 0;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+			nw++;
+	}
+
+	p = malloc(sizeof(char *) * (nw + 1));
+
+	for (i = 0; i < nw; i++)
+	{
+		while (str[k] == ' ')
+			k++;
+
+		for (j = 0; str[k] != ' '; k++)
+			j++;
+
+		k -= j;
+
+		p[i] = malloc(sizeof(char) * (j + 1));
+
+		if (p[i] == NULL)
+			return (NULL);
+
+		for (j = 0; str[k] != ' '; j++, k++)
+			p[i][j] = str[k];
+
+		p[i][j] = '\0';
+	}
+
+	p[i] = NULL;
+
+	return (p);
+}
