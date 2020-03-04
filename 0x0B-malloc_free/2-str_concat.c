251f8052d6c622124d466bdf1f1eb144ff0496e5
Sean Taylor <silverwolf4350@gmail.com>
2020-03-04 14:26:00 +0000

    Changed handling of NULL arguments.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,13 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, len = 0, len2 = 0;
 	char *p;
+	char *p1 = "";
+	char *p2 = "";
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = p1;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = p2;
 
 	while (s1[len])
 		len++;
