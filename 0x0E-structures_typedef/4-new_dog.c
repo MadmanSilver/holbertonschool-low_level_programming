5d69020fd0bd4525f93c9c882e9a511951dd291a
Sean Taylor <silverwolf4350@gmail.com>
2020-03-09 20:49:14 +0000

    Added checks and frees.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,42 +11,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
-	int nc = 0;
-	int oc = 0;
+	int i, nc = 0, oc = 0;
 	char *n;
 	char *o;
 	dog_t *d;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[nc])
 		nc++;
 	while (owner[oc])
 		oc++;
-
 	n = malloc(sizeof(char) * (nc + 1));
 	o = malloc(sizeof(char) * (oc + 1));
-
 	if (n == NULL || o == NULL)
+	{
+		free(n);
+		free(o);
 		return (NULL);
-
+	}
 	for (i = 0; i < nc; i++)
 		n[i] = name[i];
 	n[i] = '\0';
 	for (i = 0; i < oc; i++)
 		o[i] = owner[i];
 	o[i] = '\0';
-
 	d = malloc(sizeof(struct dog));
-
 	if (d == NULL)
+	{
+		free(n);
+		free(o);
+		free(d);
 		return (NULL);
-
+	}
 	d->name = n;
 	d->age = age;
 	d->owner = o;
-
 	return (d);
 }
