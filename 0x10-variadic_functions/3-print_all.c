7f7433f538d54ef0caf86b8acc8ba0f8f3f24b88
Sean Taylor <silverwolf4350@gmail.com>
2020-03-14 22:16:14 +0000

    Added separators.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,39 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, count = 0;
+	int i = 0, count = 0, first = 1;
 	char *s;
 	va_list args;
 
-	if (format != NULL && format[i] != '\0')
+	while (format != NULL && format[count] != '\0')
+		count++;
+	va_start(args, format);
+	while (i < count)
 	{
-		while (format[count] != '\0')
-			count++;
-		va_start(args, format);
-		while (i < count)
+		if (!(first) && (format[i] == 'c' || format[i] == 'i'
+			|| format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				case 's':
-					s = va_arg(args, char *);
-					if (s == NULL)
-						s = "(nil)";
-					printf("%s", s);
-					break;
-				default:
-					break;
-			}
-			i++;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				first = 0;
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				first = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				first = 0;
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				first = 0;
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				break;
 		}
+		i++;
 	}
 	printf("\n");
 	va_end(args);
