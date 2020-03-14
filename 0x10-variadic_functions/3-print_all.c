bb3bfe3776e7126852c15b9e63f04702447a85b2
Sean Taylor <silverwolf4350@gmail.com>
2020-03-14 21:24:53 +0000

    Removed unnecessary code and added format null check.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,41 +8,39 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, count = 0, sub = 0;
+	int i = 0, count = 0;
 	char *s;
 	va_list args;
 
-	while (format[count] != '\0')
+	if (format != NULL)
 	{
-		count++;
-		if (format[count] != 'c' && format[count] != 'i' &&
-			format[count] != 'f' && format[count] != 's')
-			sub++;
-	}
-	va_start(args, format);
-	while (i < count)
-	{
-		switch (format[i])
+		while (format[count] != '\0')
+			count++;
+		va_start(args, format);
+		while (i < count)
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s", s);
-				break;
-			default:
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s", s);
+					break;
+				default:
+					break;
+			}
+			i++;
 		}
-		i++;
 	}
 	printf("\n");
 	va_end(args);
