dd939c41dcf359f85e8101083170ecc2f4ae1830
Sean Taylor <silverwolf4350@gmail.com>
2020-03-20 02:30:08 +0000

    Removed unused variable.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i, count = 0;
+	unsigned int count = 0;
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL || str == NULL)
