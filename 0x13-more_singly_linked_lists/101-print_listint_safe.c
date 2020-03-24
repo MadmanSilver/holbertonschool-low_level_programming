ab7defb49c82b43f89e26b72291865edf2d4b7da
Sean Taylor <silverwolf4350@gmail.com>
2020-03-24 15:13:02 +0000

    Modified array size and intitialization.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,16 +16,16 @@ size_t print_listint_safe(const listint_t *head)
 	if (head == NULL || head->next == NULL)
 		exit(98);
 
-	a = malloc(sizeof(listint_t *) * 1024);
+	a = malloc(sizeof(listint_t *) * 2024);
 	p = head->next;
 
 	if (a == NULL)
 		exit(98);
 
 	printf("[%p] %d\n", (void *)head, head->n);
-	for (i = 1; p != NULL; i++)
+	for (i = 0; p != NULL; i++)
 	{
-		for (j = 1; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			if (p == a[j] || p == head)
 			{
