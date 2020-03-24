4f0f5466483b2b1378d36c18b74488cbf3b8c20f
Sean Taylor <silverwolf4350@gmail.com>
2020-03-24 14:37:45 +0000

    Fixed index 0 always having next = NULL.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,10 +34,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	if (i == 0 )
+	if (i == 0)
 	{
 		*head = new;
-		new->next = NULL;
+		new->next = p;
 	}
 	else
 	{
