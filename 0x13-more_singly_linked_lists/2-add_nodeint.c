c8dd19cf5f5c754d5dee1be2ddd7bd56bdddc39b
Sean Taylor <silverwolf4350@gmail.com>
2020-03-24 14:16:00 +0000

    Added NULL catches.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,10 +11,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	if (new == NULL || head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->next = *head;
 	*head = new;
