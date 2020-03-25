b9aa8086005e18d64ac57f572fa105429411995e
Sean Taylor <silverwolf4350@gmail.com>
2020-03-25 15:55:24 +0000

    Fixed one looping node case.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,13 +16,14 @@ size_t print_listint_safe(const listint_t *head)
 	if (head == NULL)
 		return (0);
 
-	a = malloc(sizeof(listint_t *) * 2024);
+	a = malloc(sizeof(listint_t *) * 1024);
 	p = head->next;
 
 	if (a == NULL)
 		exit(98);
 
-	printf("[%p] %d\n", (void *)head, head->n);
+	if (head != head->next)
+		printf("[%p] %d\n", (void *)head, head->n);
 	for (i = 1; p != NULL; i++)
 	{
 		for (j = 1; j < i; j++)
