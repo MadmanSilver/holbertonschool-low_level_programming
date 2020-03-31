10c54be781d28cb1bf05700c3eea87aa2e101aab
Sean Taylor <silverwolf4350@gmail.com>
2020-03-31 23:47:34 +0000

    Moved all error handling into a seperate function to save lines.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include "holberton.h"
 #include <stdio.h>
 
@@ -14,38 +15,64 @@ int main(int argc, char **argv)
 	char *buff = malloc(sizeof(char *) * 1024);
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		fail('u', -1, -1);
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		fail('r', file_to, file_from, argv[1]);
 	len = read(file_from, buff, 1024);
 	if (file_to == -1 || write(file_to, buff, len) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		fail('w', file_to, file_from, argv[2]);
 	while (len == 1024)
 	{
 		len = read(file_from, buff, 1024);
 		write(file_to, buff, len);
 	}
 	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+		fail('c', file_to, file_from, file_to);
 	if (close(file_from) == -1)
+		fail('r', file_to, file_from, file_from);
+	return (0);
+}
+
+/**
+ * fail - handles errors
+ * @c: reason for error
+ * @f1: first file to close
+ * @f2: second file to close
+ */
+void fail(char c, int f1, int f2, ...)
+{
+	va_list args;
+
+	va_start(args, f2);
+	close(f1);
+	close(f2);
+
+	if (c == 'c')
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", va_arg(args, int));
+		va_end(args);
 		exit(100);
 	}
-	return (0);
+	else if (c == 'w')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", va_arg(args, char *));
+		va_end(args);
+		exit(99);
+	}
+	else if (c == 'r')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			va_arg(args, char *));
+		va_end(args);
+		exit(98);
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		va_end(args);
+		exit(97);
+	}
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,5 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void fail(char c, int f1, int f2, ...);
 
 #endif
