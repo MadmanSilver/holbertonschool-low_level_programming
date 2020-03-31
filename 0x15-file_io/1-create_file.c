15f94badb98e3e68e1277ff3118965b6a80bd47f
Sean Taylor <silverwolf4350@gmail.com>
2020-03-31 22:54:51 +0000

    Added closing statements.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,10 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file == -1)
+	{
+		close(file);
 		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
@@ -26,7 +29,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (write(file, text_content, len) == -1)
+	{
+		close(file);
 		return (-1);
+	}
+
+	close(file);
 
 	return (1);
 }
