1731d9e8d0a7925048a468b8b4253cffc4b4f9c6
Sean Taylor <silverwolf4350@gmail.com>
2020-03-31 22:38:30 +0000

    Added file closing statements.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,15 +9,33 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file = open(filename, O_RDONLY);
+	int file;
 	char *s = malloc(sizeof(char) * letters);
-	ssize_t rd = read(file, s, letters);
+	ssize_t rd;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	rd = read(file, s, letters);
 
 	if (rd == -1)
+	{
+		close(file);
 		return (0);
+	}
 
 	if (write(STDOUT_FILENO, s, letters) == -1)
+	{
+		close(file);
 		return (0);
+	}
+
+	close(file);
 
 	return (rd);
 }
