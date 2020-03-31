a16bea585582d1bd2a9f1632cc7574776971eb34
Sean Taylor <silverwolf4350@gmail.com>
2020-03-31 22:58:28 +0000

    Added closing statements.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,13 +20,21 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		close(file);
 		return (1);
+	}
 
 	while (text_content[len])
 		len++;
 
 	if (write(file, text_content, len) == -1)
+	{
+		close(file);
 		return (-1);
+	}
+
+	close(file);
 
 	return (1);
 }
