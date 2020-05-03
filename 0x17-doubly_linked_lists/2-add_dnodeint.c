6c34e2e5a8923c98687a9c74691abed0b1b3e7a2
Sean Taylor <silverwolf4350@gmail.com>
2020-05-03 21:40:21 +0000

    Changing workstations.

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the begginging of a dlistint_t list
+ * @head: head of list to add to
+ * @n: value of the node to add
+ *
+ * Return: address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	
+}
