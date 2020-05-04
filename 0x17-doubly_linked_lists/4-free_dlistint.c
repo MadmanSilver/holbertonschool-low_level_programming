c1a0751327da3dfa9b3ceb3ac039165229cafa27
Sean Taylor <silverwolf4350@gmail.com>
2020-05-04 02:57:18 +0000

    Removed unnecessary loop and ensured one-item lists get freed.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,8 +8,11 @@ void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
 		return;
-	while (head->prev != NULL)
-		head = head->prev;
+	if (head->next == NULL)
+	{
+		free(head);
+		return;
+	}
 	head = head->next;
 	while (head != NULL)
 	{
