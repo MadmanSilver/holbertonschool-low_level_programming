03bcd5ca2447a5330d0288df3d444af3bc5e7462
Sean Taylor <silverwolf4350@gmail.com>
2020-07-06 01:55:38 +0000

    Created all files for mandatory tasks.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+""" Imma measure an island! """
+
+def island_perimeter(grid):
+    """ This mah brain! """
+    y = 0
+    x = 0
+    perim = 2
+    direction = 2 # N = 0, E = 1, S = 2, W = 3
+
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x]:
+                break
+        if x + 1 != len(grid[0]):
+            break
+
+    if y + 1 == len(grid) and x + 1 == len(grid[0]):
+        return 0
+
+    prev_y = y
+    prev_x = x
+
+    if x + 1 < len(grid[0]) and grid[y][x + 1]:
+        x += 1
+        direction = 1
+    elif y + 1 < len(grid) and grid[y + 1][x]:
+        perim += 1
+        y += 1
+        direction = 2
+    elif x - 1 >= 0 and grid[y][x - 1]:
+        perim += 2
+        x -= 1
+        direction = 3
+    else:
+        perim += 3
+        y -= 1
+        direction = 0
+
+    while x != prev_x or y != prev_y:
+        if direction == 0:
+            if x - 1 >= 0 and grid[y][x - 1]:
+                x -= 1
+                direction = 3
+            elif y - 1 >= 0 and grid[y - 1][x]:
+                perim += 1
+                y -= 1
+                direction = 0
+            elif x + 1 < len(grid[0]) and grid[y][x + 1]:
+                perim += 2
+                x += 1
+                direction = 1
+            else:
+                perim += 3
+                y += 1
+                direction = 2
+        elif direction == 1:
+            if y - 1 >= 0 and grid[y - 1][x]:
+                y -= 1
+                direction = 0
+            elif x + 1 < len(grid[0]) and grid[y][x + 1]:
+                perim += 1
+                x += 1
+                direction = 1
+            elif y + 1 < len(grid) and grid[y + 1][x]:
+                perim += 2
+                y += 1
+                direction = 2
+            else:
+                perim += 3
+                x -= 1
+                direction = 3
+        elif direction == 2:
+            if x + 1 < len(grid[0]) and grid[y][x + 1]:
+                x += 1
+                direction = 1
+            elif y + 1 < len(grid) and grid[y + 1][x]:
+                perim += 1
+                y += 1
+                direction = 2
+            elif x - 1 >= 0 and grid[y][x - 1]:
+                perim += 2
+                x -= 1
+                direction = 3
+            else:
+                perim += 3
+                y -= 1
+                direction = 0
+        else:
+            if y + 1 < len(grid) and grid[y + 1][x]:
+                y += 1
+                direction = 2
+            elif x - 1 >= 0 and grid[y][x - 1]:
+                perim += 1
+                x -= 1
+                direction = 3
+            elif y - 1 >= 0 and grid[y - 1][x]:
+                perim += 2
+                y -= 1
+                direction = 0
+            else:
+                perim += 3
+                x += 1
+                direction = 1
+
+    return perim
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,1 @@
+0x1C C - Make files is a Holberton project to teach about make files in the C programming language.
