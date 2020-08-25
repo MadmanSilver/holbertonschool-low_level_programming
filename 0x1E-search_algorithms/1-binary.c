2d3b341c8b2c436c794ed806f7ad528fc42a2d6d
Sean Taylor <silverwolf4350@gmail.com>
2020-08-25 21:40:43 +0000

    Created all files for mandatory tasks.

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary search algorithm
+ * @array: pointer to array to search
+ * @size: size of array to search
+ * @value: value to search for
+ *
+ * Return: index of found element, -1 if none
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int i = size / 2, lo = 0, hi = size, j;
+
+	for (; lo < hi; i = (hi + lo) / 2)
+	{
+		if ((hi - lo) % 2 == 0)
+			i--;
+		printf("Searching in array: ");
+		for (j = lo; j < hi - 1; j++)
+			printf("%d, ", j);
+		printf("%d\n", j);
+		if (array[i] == value)
+			return (i);
+		else if (array[i] < value)
+			lo = i + 1;
+		else
+			hi = i;
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+0x1E C - Search algorithms is a Holberton project to teach about search algorithms in the C programming language.
